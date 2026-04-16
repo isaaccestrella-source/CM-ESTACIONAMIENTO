26111aa9025c956218ce003975075325e3e8788a
isaaccestrella-source <isaaccestrella@gmail.com>
2026-04-16 01:44:17 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,15 @@
 const admin = require("firebase-admin");
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.includes("\\n")
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  : process.env.FIREBASE_PRIVATE_KEY;
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey,
     }),
   });
 }
